723ab548d8cf14c9f5ccce2483743b5e3d766284
Koustav Das <107167244+algoSiliguri@users.noreply.github.com>
2023-03-14 20:08:33 +0000

    Code modularity
    Made code much more modular for better visibility and readability.

--- metrics/data_loaders/financial_metric_load.py
+++ metrics/data_loaders/financial_metric_load.py
@@ -1,5 +1,6 @@
 from openbb_terminal.sdk import openbb
 import os
+from typing import List
 import pandas as pd
 import yfinance as yf
 
@@ -9,33 +10,70 @@ if 'test' not in globals():
     from mage_ai.data_preparation.decorators import test
 
 
-def get_ticker_list(counter):
+def read_ticker_list() -> List[str]:
+    """
+    Reads a list of ticker symbols from a CSV file.
 
+    Returns:
+        A list of ticker symbols.
+    """
     # specify the relative or absolute path of the file
     file_path = 'tickers.csv'
     # get the absolute path of the file
     absolute_path = os.path.abspath(file_path)
     df = pd.read_csv(absolute_path)
-    ticker = df['Symbol'].loc[counter:]
-    return ticker.to_list()
+    return df['Symbol'].tolist()
 
-def get_pct_change_ticker(ticker):
+def get_stock_returns(ticker: str, years: int) -> List[float]:
+    """
+    Calculates the percent change of a stock's price over the last `years` years.
 
+    Args:
+        ticker: The stock ticker symbol.
+        years: The number of years to calculate the percent change over.
+
+    Returns:
+        A list of yearly stock returns as floats.
+    """
     start_date = "2002-01-01"
     end_date = "2022-12-31"
-
     stock_data = yf.download(ticker, start=start_date, end=end_date)
-    yearly_prices = stock_data["Adj Close"].resample("Y").last()
-    year_range = pd.date_range(start=start_date, end=end_date, freq='Y')
-    yearly_prices = yearly_prices.reindex(year_range)
-    yearly_prices = yearly_prices.ffill()
-    yearly_returns = yearly_prices.pct_change().fillna(0)
-    yearly_returns.drop(yearly_returns.index[0], inplace=True)
+    if stock_data is None:
+        print(f"Error: Could not retrieve data for {ticker}.")
+        return []
+    yearly_prices = stock_data["Adj Close"].resample("Y").last().ffill()
+    yearly_returns = yearly_prices.pct_change().fillna(0)[1:]
+    return yearly_returns.tolist()[-years:]
 
-    return yearly_returns.to_list()
+def get_growth_ratios(ticker: str, years: int) -> pd.DataFrame:
+    """
+    Retrieves the financial growth ratios for a stock over the last `years` years.
+
+    Args:
+        ticker: The stock ticker symbol.
+        years: The number of years to retrieve the growth ratios for.
+
+    Returns:
+        A DataFrame containing the growth ratios for the stock.
+    """
+    df = openbb.stocks.fa.growth(ticker, limit=years)
+    if len(df) < 5:
+        print(f"Skipping {ticker} due to insufficient data")
+        return pd.DataFrame()
+    df = df.T.drop('Period', axis=1)
+    df['Year'] = df.index
+    df = df.reset_index(drop=True)
+    df['Ticker'] = [ticker for i in range(0, len(df))]
+    return df
 
 @data_loader
-def get_ratio_growth_stocks():
+def get_ratio_growth_stocks() -> pd.DataFrame:
+    """
+    Retrieves the financial growth ratios and stock returns for a list of ticker symbols.
+
+    Returns:
+        A DataFrame containing the growth ratios and stock returns for the tickers.
+    """
     api_limit = 10
     counter = 0
     run_count = 0
@@ -44,32 +82,30 @@ def get_ratio_growth_stocks():
         with open('last_processed_counter.txt', 'r') as f:
             counter = int(f.read().strip())
 
-
     lst_stock_df = []
-    # obb = openbb.stocks.fa.growth()
-    
-    for tick in get_ticker_list(counter):
-        df_growth_ratio = openbb.stocks.fa.growth(tick, limit=20)
-        # if len(df_growth_ratio) != 20:
-        #     print(f"Skipping {tick} due to unexpected data length")
-        #     continue
-        df_growth_ratio = df_growth_ratio.T.drop('Period', axis=1)
-        df_growth_ratio['Year'] = df_growth_ratio.index
-        df_growth_ratio = df_growth_ratio.reset_index(drop=True)
-        df_growth_ratio['Stock Percent Change'] = get_pct_change_ticker(tick)
-        df_growth_ratio['Ticker'] = [
-            tick for i in range(0, len(df_growth_ratio.index))]
+
+    for ticker in read_ticker_list()[counter:]:
+        df_growth_ratio = get_growth_ratios(ticker, 20)
+        if df_growth_ratio.empty:
+            continue
+        stock_returns = get_stock_returns(ticker, len(df_growth_ratio))
+        if not stock_returns:
+            print(f"Insufficient data for {ticker} to calculate percent change")
+            continue
+        df_growth_ratio['Stock Percent Change'] = stock_returns
         lst_stock_df.append(df_growth_ratio)
+
         counter += 1
         run_count += 1
-        
+
         if run_count == api_limit-1:
             with open('last_processed_counter.txt', 'w') as f:
                 f.write(str(counter))
             print("Closing API limit, Successful Run")
             break
-                       
+
     return pd.concat(lst_stock_df)
+
     
     
 @test
